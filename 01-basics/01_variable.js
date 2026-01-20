a7d47328b767f4a34bcc155da02159ba7b85c9d8
Varun Kumar <86644569+varunpanjiyar@users.noreply.github.com>
2026-01-20 08:38:59 +0000

    letVarConst

---
+++ 01-basics/01_variable.js
@@ -0,0 +1,8 @@
+const accountId = 12345
+let emailid = "varunpanjiyar@gmail.com"
+let password = "1234"
+
+//accountId  =2 this is wrong we can't reassign the varible coz it assiasn to constant
+
+console.log(accountId);
+
