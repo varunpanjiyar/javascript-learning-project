6a381e75c03b8444e2afcec4572d57d03b1961a8
Varun Kumar <86644569+varunpanjiyar@users.noreply.github.com>
2026-01-22 06:22:43 +0000

    let investigation

--- 01-basics/01_variable.js
+++ 01-basics/01_variable.js
@@ -1,8 +1,16 @@
 const accountId = 12345
-let emailid = "varunpanjiyar@gmail.com"
+let emailId = "varunpanjiyar@gmail.com"
 let password = "1234"
 
 //accountId  =2 this is wrong we can't reassign the varible coz it assiasn to constant
 
 console.log(accountId);
+
+
+//we can update let there i see that let can update the varibel
+ emailId = "arunkumarpanjiyaer@gmail.com"
+ console.log(emailId);
+ 
+
+
 
