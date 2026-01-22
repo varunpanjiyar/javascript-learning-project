ee0a69391faa2c3dd2b94b8bdbba0a5a6c9f2950
Varun Kumar <86644569+varunpanjiyar@users.noreply.github.com>
2026-01-22 15:12:09 +0000

    let,var,const

--- 01-basics/01_variable.js
+++ 01-basics/01_variable.js
@@ -1,8 +1,16 @@
 const accountId = 12345
 let emailId = "varunpanjiyar@gmail.com"
-let password = "1234"
+var password = "1234"
+accountCity = "Bangluru" 
+password = "321"
 
-//accountId  =2 this is wrong we can't reassign the varible coz it assiasn to constant
+/* 
+prefer not use to var
+ because there is issue in block scope and function scope
+*/
+let accountState;
+
+//accountId  =2 this is wrong we can't reassign the varible coz it assign to constant
 
 console.log(accountId);
 
@@ -10,7 +18,7 @@ console.log(accountId);
 //we can update let there i see that let can update the varibel
  emailId = "arunkumarpanjiyaer@gmail.com"
  console.log(emailId);
- 
+ console.table([accountId,emailId,password,accountCity,accountState])
 
 
 
