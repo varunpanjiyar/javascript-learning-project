e84197f47009ebcfeb99cf958ed5770d9eeb5341
Varun Kumar <86644569+varunpanjiyar@users.noreply.github.com>
2026-01-24 09:36:01 +0000

    dataype

--- 01-basics/02_dataType.js
+++ 01-basics/02_dataType.js
@@ -1,3 +1,29 @@
-console.log(3+3)
 
-console.log("varun")
+
+let name = "varun"// string
+let number  = 18
+let isLoggedIn = "false"// boolean datatype
+let state; // this is undifined here i declear the variable not assign the value
+
+// console.log(typeof "name");
+console.log(typeof null) // types of  null always gives object datatype
+console.log(typeof undefined);
+
+
+
+//there are two type of datatpe 
+// 1st is primitive datatype . there is 7 primitive data type
+
+// number = 2 to power 53
+// string  = ""
+//bigInt =. bascially it use for large number
+//boolean = true or false
+// null = standalone value
+//undifined = when we declear the vaiable but assign the number 
+// symbol = it is use for unqiue we more use about symbol in react
+
+
+//non primitive-datatpe
+// there are two type of non-primitive type 
+// 1 object
+
