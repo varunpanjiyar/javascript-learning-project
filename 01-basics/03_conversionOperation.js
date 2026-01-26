da733165c62c27e92f557f172aa67b025ed30a38
Varun Kumar <86644569+varunpanjiyar@users.noreply.github.com>
2026-01-26 17:23:24 +0000

    conversion in datatype

---
+++ 01-basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+// // let mark = 33;
+// console.log(typeof mark);
+// console.log(typeof (mark))
+
+// let mark = "33abc"
+// let convertNumber = Number(mark)
+// console.log(typeof convertNumber)
+
+// let score  = null;
+// console.log(typeof score) // it gives object
+
+// let  number = 'undefined';
+// console.log(typeof score)
+
+
+let city = '3abc'
+
+console.log(typeof city)
+
+let convertInNumber = Number(city)
+console.log(typeof convertInNumber);
+console.log(convertInNumber)
