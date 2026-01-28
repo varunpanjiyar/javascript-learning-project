4eadd5550a44f1a64da0241dd2360689dd88c7a7
Varun Kumar <86644569+varunpanjiyar@users.noreply.github.com>
2026-01-28 16:03:55 +0000

    date-time

---
+++ 01-basics/07_date_time.js
@@ -0,0 +1,40 @@
+let mydate = new Date()
+// console.log(mydate)
+// console.log(mydate.toLocaleString())
+// console.log(mydate.toDateString())
+// console.log(mydate.toLocaleDateString());
+// console.log(mydate.getDate());
+// console.log(mydate.getDay());
+// console.log(mydate.getFullYear());
+// console.log(mydate.getMinutes());
+
+
+// let createDate  =  new Date(2016, 0, 12,9,5)
+// console.log(createDate.toLocaleString());
+// console.log(createDate.toDateString()); 
+
+
+
+let createdate = new Date("2022-2-13")
+// console.log(createdate.toLocaleString());
+
+// let myTimeStamp =  Date.now()
+// console.log(myTimeStamp)
+// console.log(createdate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth());
+
+
+
+
+
+
+
+
+
+
+
