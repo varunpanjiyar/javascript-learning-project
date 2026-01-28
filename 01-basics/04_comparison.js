096f5ef8ed172709f6113343de95fc6efbc83936
Varun Kumar <86644569+varunpanjiyar@users.noreply.github.com>
2026-01-28 09:24:38 +0000

    compariosn

---
+++ 01-basics/04_comparison.js
@@ -0,0 +1,14 @@
+// console.log(1>2);
+// console.log(2==3);
+// console.log(3<2)
+// console.log(5>=5)
+
+
+console.log("3">2) // beacause it treat string like a number that's why give true output
+
+
+console.log("3"===3) //this is strict checker it check the value and the datatype also that's why it gives flase 
+
+console.log("3"==="3")
+
+//  difference between == and ===,     double equal to check only number and tripple equal to ckeck number and datatype also if both are not equal it gives false    
