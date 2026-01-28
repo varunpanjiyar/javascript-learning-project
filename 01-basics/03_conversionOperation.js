c34a2a877ad3996321dc4b5309606ee57fe65499
Varun Kumar <86644569+varunpanjiyar@users.noreply.github.com>
2026-01-28 09:05:57 +0000

    mathimatical operation

--- 01-basics/03_conversionOperation.js
+++ 01-basics/03_conversionOperation.js
@@ -18,5 +18,38 @@ let city = '3abc'
 console.log(typeof city)
 
 let convertInNumber = Number(city)
-console.log(typeof convertInNumber);
-console.log(convertInNumber)
+// console.log(typeof convertInNumber);
+// console.log(convertInNumber)
+
+// mathematical operation
+
+// console.log(2+2);
+// console.log(3-2);
+// console.log(3*3);
+// console.log(3**2); //power 
+// console.log(5/2);
+// console.log(3%2) 
+
+
+// console.log(3+"2")
+// console.log("3"+2);
+// console.log("3"+2+2)
+// console.log(2+2+"2");
+
+
+let score = 100
+score++ // this is postfix
+console.log(score);
+
+
+
+
+
+
+
+
+
+
+
+
+
