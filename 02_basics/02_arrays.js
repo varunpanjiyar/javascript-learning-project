763a611875c0a02ae12978d4f79df293f82caecb
Varun Kumar <86644569+varunpanjiyar@users.noreply.github.com>
2026-01-29 15:53:01 +0000

    arrays method

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,17 @@
+let bikeCompany = ["hero", "splender", "honda", "tvs"]
+let carCampany = ["bmw", "mahindra", "tata", "tesla"]
+
+// bikeCompany.push(carComapny)
+// console.log(bikeCompany); // this is bad practice 
+
+// concat()=> it merger two or more array in new array 
+
+let newArray = bikeCompany.concat(carCampany)
+console.log(newArray);
+
+
+// flat ()=> it add two or more than two array in new array vairiabe;
+
+let anotherArray = bikeCompany.flat()
+
+
