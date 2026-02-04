cd24f31e1295833bb573dbfd0c9fc8d42aeac42d
Varun Kumar <86644569+varunpanjiyar@users.noreply.github.com>
2026-02-04 08:50:42 +0000

    singlton,literal

--- 02_basics/03_object.js
+++ 02_basics/03_object.js
@@ -1,11 +1,46 @@
-let object ={
-    name: "varun",
-    age:24,
-    gmail : "varunPnajiyar@gmail.com"
-}
 
-console.log(object.name);
-console.log(object.age);
-console.log(object.gmail);
+// Singleton: An object created using Object.create() is called a singleton object.
+// const obj = Object.create({});
+
+
+//question=>  make a sybmbol  and refer to the object and print the value 
+
+let mySymbol = Symbol("key1")
+
+
+//object litreal
+let object = {
+    name: "varun",
+    // mySymbol: "mykey1", // this is not a symbol
+    [mySymbol]: "key1",
+    "last Name": "kumar",
+    age: 24,
+    gmail: "varunPnajiyar@gmail.com",
+    isLoggedin: false,
+    presentPastWeek: ["monday", "tuesday", "thursday"]
+
+
+}
+console.log(object);
+
+
+// console.log(object.name);// i can access like this but other method
+// console.log(object.last Name);// here i can't acess cox this syntax is not allow
+// console.log(object["last Name"]);// this is correct syntax to access values
+// console.log(typeof object[mySymbol]); // it show string not symbol no need to rape in string
+
+object.greeting = function () {
+    console.log("hello js learner"); // here i reffer function to the object
+}
+console.log(object.greeting());
+
+object.greetingTwo = function(){
+    console.log(`my  name is: ${this.name}`)
+}
+console.log(object.greetingTwo());
+
+
+
+
 
 
