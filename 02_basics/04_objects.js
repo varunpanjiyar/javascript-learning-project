d906ed595725f6d8ff23a3ec9f907fb793caa0ac
Varun Kumar <86644569+varunpanjiyar@users.noreply.github.com>
2026-02-04 12:50:15 +0000

    object property

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,82 @@
+// Singleton: An object created using the constructor (new Object()) is called a singleton object.
+
+const MySinglton = new Object() // singleton object
+
+
+// object litreal=> an object litrel is simplest way to create an object with using {} without using constructor
+let tinderUseer = {} // object literal
+
+tinderUseer.id = "123abc"
+tinderUseer.name = "varun"
+tinderUseer.photo = true
+
+// console.log(tinderUseer);
+
+// nested object
+
+let socity = {
+    scocityName: "supertech",
+    socityMiddleName: {
+        socityFullName: {
+            middlename: "eco",
+            lastName: "village1"
+        }
+    }
+}
+// console.log(socity.socityMiddleName.socityFullName.lastName);
+
+
+
+let obj = { a: 1, b: 2, c: 3 }
+let obj1 = { d: 4, e: 5, f: 6 }
+// let newObj = {obj,  obj1}
+// console.log(newObj);
+
+// Object.assign() merges two or more objects into a new object
+
+let newObj1 = Object.assign({}, obj1, obj)
+// console.log(newObj1);
+
+// spread=> it broke the all elemnt and store in new varibale
+let newArray = { ...obj, ...obj1 }
+// console.log(newArray);
+
+
+// array object
+
+let arrayObject = [
+    {
+        id: "123abc",
+        name :"varun"
+    },
+    {
+        id:"23bc",
+        name :"manish"
+    },
+]
+// console.log(arrayObject[0]);
+
+let anotherUser = {
+    name :"varun",
+    lastname  : "kumar",
+    gmail:"varun@gmail.com",
+    isLoggedIn: false,
+}
+
+console.log(Object.keys(anotherUser));// here i find key of object or anotherUser
+console.log(Object.values(anotherUser));// here i find the values of anotheruser object
+console.log(Object.entries(anotherUser));// it store in array and give key and value sepratly
+console.log(anotherUser.hasOwnProperty("gmail")); // fro asking question that key is aviable in object or not
+
+
+
+
+
+
+
+
+
+
+
+
+
