5a2a9124ad573cf22720cab648748eac28c5d46f
Varun Kumar <86644569+varunpanjiyar@users.noreply.github.com>
2026-02-07 15:18:42 +0000

    function

---
+++ 03_basics/02_function.js
@@ -0,0 +1,6 @@
+function userMassage(username='please enter username'){ // here i can add default value if username is not given
+return `${username}, is my username`
+}
+// console.log(userMassage("itz.varun09"));
+console.log(userMassage());
+
