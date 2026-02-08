dfb20989c1112f5912653add4bb2f2bbae44119c
Varun Kumar <86644569+varunpanjiyar@users.noreply.github.com>
2026-02-08 10:01:43 +0000

    problem solving

---
+++ question solve/01_objectQuestion.js
@@ -0,0 +1,24 @@
+// Convert Object to Array
+
+let obj = {
+  a: 1,
+  b: 2,
+  c: 3
+};
+
+console.log(Object.entries(obj));
+
+// Sum Values Find total salary.
+let salaries = {
+  john: 1000,
+  alex: 2000,
+  mike: 1500
+};
+
+const{john,alex,mike} = salaries
+console.log(john+alex+mike);
+
+
+// console.log(`${salaries.john} +${salaries.alex}+ ${salaries.mike}`);
+
+
