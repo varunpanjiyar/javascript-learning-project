d4edded01fe993a6b8447fb6338fdf906bd2e3f8
Varun Kumar <86644569+varunpanjiyar@users.noreply.github.com>
2026-02-09 12:00:00 +0000

    question solve

--- question solve/01_objectQuestion.js
+++ question solve/01_objectQuestion.js
@@ -6,7 +6,7 @@ let obj = {
   c: 3
 };
 
-console.log(Object.entries(obj));
+// console.log(Object.entries(obj));
 
 // Sum Values Find total salary.
 let salaries = {
@@ -15,10 +15,124 @@ let salaries = {
   mike: 1500
 };
 
-const{john,alex,mike} = salaries
-console.log(john+alex+mike);
+const { john, alex, mike } = salaries
+// console.log(john+alex+mike);
 
 
-// console.log(`${salaries.john} +${salaries.alex}+ ${salaries.mike}`);
+// 👉 Find subject with highest marks.
+let marks = {
+  math: 88,
+  english: 75,
+  science: 92,
+  computer: 85
+};
+const maxNumber = Math.max(...Object.values(marks))
+// console.log(maxNumber);
+for (const subject in marks) {
+  if (marks[subject] === maxNumber) {
+    // console.log(`${subject} = ${maxNumber}`);
+
+  }
+}
+
+// Convert Object to Array of Objects
+let user = {
+  name: "Varun",
+  age: 22,
+  city: "Delhi"
+};
+
+
+let result = Object.entries(user).map(([key, value]) => ({
+  key,
+  value
+}));
+
+// console.log(result);
+// console.log(`key:- ${Object.keys(user)} and value:-${Object.values(user)}`);
+
+
+// 4️⃣ Remove Property Dynamically
+let product = {
+  name: "Mobile",
+  price: 15000,
+  color: "Black"
+};
+// destructuring
+const { color, ...rest } = product
+// console.log(rest);
+
+let key = "color";
+delete product[key];
+// console.log(product);
+// let convertArray=Object.entries(product)
+
+// console.log(convertArray.pop());
+// console.log(convertArray);
+// console.log(product);
+
+
+// Find how many properties are in this object:
+
+let mobile = {
+  brand: "Samsung",
+  price: 20000,
+  color: "Black"
+};
+// console.log(Object.entries(mobile).length);
+
+
+// 8️⃣ Convert Object to Array
+let obj1 = {
+  a: 1,
+  b: 2,
+  c: 3
+};
+// console.log(Object.entries(obj1));
+
+
+// Convert Object → Array of Keys
+// 👉 Convert this object into an array of keys.
+let person = {
+  name: "Amit",
+  age: 25,
+  city: "Noida"
+};
+
+// console.log(Object.entries(person));
+
+
+
+
+// Count Values Greater Than 50
+let numbers = {
+  a: 40,
+  b: 60,
+  c: 90,
+  d: 20
+};
+
+const findNumber= Object.entries(numbers)
+for(const number in numbers)
+if(findNumber>50){
+  console.log(findNumber)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
