209a49d47609d82750d47eff345986e090723f42
Varun Kumar <86644569+varunpanjiyar@users.noreply.github.com>
2026-02-10 11:34:51 +0000

    switch statement

---
+++ 04_controlFlow/switch.js
@@ -0,0 +1,43 @@
+let month = 3;
+switch (month) {
+    case 1:
+        console.log("january")
+        break;
+    case 2:
+        console.log("fabrary")
+        break;
+    case 3:
+        console.log("march")
+        break;
+    case 4:
+        console.log("april")
+        break;
+
+    default:
+        console.log("dont match anything");
+
+        break;
+}
+
+// let compare  string
+
+let monthName = "april"
+switch (monthName) {
+    case "jan":
+        console.log("jan");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+    default:
+        console.log("don't meet anything");
+
+        break;
+}
+
