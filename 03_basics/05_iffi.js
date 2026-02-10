d5943327f926b75465b8ae1de6ab2ebd2962156d
Varun Kumar <86644569+varunpanjiyar@users.noreply.github.com>
2026-02-10 11:34:19 +0000

    semi colon update

--- 03_basics/05_iffi.js
+++ 03_basics/05_iffi.js
@@ -1,14 +1,14 @@
 //  IIFE (Immediately Invoked Function Expression) is used to avoid global scope pollution and it runs immediately after it is defined.
 
 //named iffi
-(function store(){
+(function store() {
     // let situation = "db-connected"
     console.log("db-connected")
 })();
 
-
+// ; is very important to run to iffi in one file  if we don't use ; break then iffi don't know where has to stop
 // unamed iffi
-( ()=>{
+(() => {
     console.log("db-connected two");
-    
+
 })()
