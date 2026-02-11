0bccc6e34b379bd6132239b7adb07bd92929c16b
Varun Kumar <86644569+varunpanjiyar@users.noreply.github.com>
2026-02-11 13:53:23 +0000

    foreach , array

---
+++ 05_iteration_forloop/05_for_eachLoop.js
@@ -0,0 +1,46 @@
+// forEach() is used to iterate over an array and access each element one by one. It provides three parameters: item (value), index, and the whole array.
+
+// basically we use for object inside the array [{}]
+
+let hero = ["batman", "superhero", "shatiman", "madmax"]
+
+// hero.forEach((value) => {
+//     console.log(value);
+
+// })
+let heros = ["batman", "superhero", "shatiman", "madmax"]
+
+hero.forEach((value) => {
+    console.log(value);
+})
+
+
+const language = ["javscript", "java", "react-js", "c++"]
+
+language.forEach((item, index, arr) => {
+    console.log(item, index, arr);
+
+})
+
+
+// aceess the items which is object is nested in array like [{}]
+
+let city = [
+    {
+        state: "bihar",
+        city: "patna"
+    },
+    {
+        state: "up",
+        city: "noida"
+    },
+    {
+        state: "punjab",
+        city: "kharar"
+    }
+]
+
+city.forEach((items) => {
+    console.log(items.city)
+
+})
